83f3529d2d32df7f15809625effc63bf1757805a
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2024-11-27 19:44:48 -0300

    Tailwind Added

--- src/main/java/br/edu/ifpb/kodak/controller/PhotographerController.java
+++ src/main/java/br/edu/ifpb/kodak/controller/PhotographerController.java
@@ -1,5 +1,10 @@
 package br.edu.ifpb.kodak.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/photographer")
 public class PhotographerController {
+
+
 
 }
