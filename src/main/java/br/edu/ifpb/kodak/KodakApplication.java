73048b59fbfa90dd26e704e90dec06b59c25e814
luna.sammuel <luna.sammuel@academico.ifpb.edu.br>
2024-12-09 12:57:30 +0000

    thymeleaf: added layout structure

--- src/main/java/br/edu/ifpb/kodak/KodakApplication.java
+++ src/main/java/br/edu/ifpb/kodak/KodakApplication.java
@@ -2,8 +2,10 @@ package br.edu.ifpb.kodak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // ignore security bcz its bothersome
 public class KodakApplication {
 
 	public static void main(String[] args) {
