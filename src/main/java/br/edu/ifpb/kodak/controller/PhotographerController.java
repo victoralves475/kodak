1ee2da6b7af85f6542b2289ba873b4d05159b4cf
buenorafa <rafaelbuenooo@outlook.com>
2025-01-16 23:43:12 -0300

    add: Sign-in controller methods

--- src/main/java/br/edu/ifpb/kodak/controller/PhotographerController.java
+++ src/main/java/br/edu/ifpb/kodak/controller/PhotographerController.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.kodak.controller;
 
 import br.edu.ifpb.kodak.model.Photographer;
 import br.edu.ifpb.kodak.service.PhotographerService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/photographer")
@@ -44,11 +47,50 @@ public class PhotographerController {
     public ModelAndView getHomePage( Photographer photographer, ModelAndView model, RedirectAttributes attr) {
         model.setViewName("/photographer/home");
         return model;
-
-
-
-
     }
+
+    @GetMapping("/sign-in")
+    public String signInPage( Photographer photographer, Model model, RedirectAttributes attr) {
+        model.addAttribute("photographer", photographer);
+        return "/photographer/sign-in";
+    }
+
+    @PostMapping("/sign-in")
+    public ModelAndView signInPage(
+            @Valid Photographer photographer,
+            BindingResult result,
+            ModelAndView model,
+            RedirectAttributes attr,
+            HttpSession session) {
+
+        if(result.hasErrors()){
+            model.setViewName("/photographer/sign-in");
+            return model;
+        }
+
+        Optional<Photographer> photographerOptional = photographerService.findByEmail(photographer.getEmail()) ;
+
+        if(photographerOptional.isEmpty()){
+            attr.addFlashAttribute("erro", "Fotógrafo não encontrado");
+            model.setViewName("redirect:/sign-in");
+            return model;
+        }
+
+        Photographer photographerSaved = photographerOptional.get();
+
+        if (!photographerService.validatePassword(photographer, photographerSaved)){
+            attr.addFlashAttribute("erro", "Senha incorreta");
+            model.setViewName("redirect:/sign-in");
+            return model;
+        }
+        attr.addFlashAttribute("mensagem", "Login realizado com sucesso!");
+
+        session.setAttribute("photographer", photographerSaved);
+
+        model.setViewName("redirect:/home");
+        return model;
+    }
+
 
 
 }
