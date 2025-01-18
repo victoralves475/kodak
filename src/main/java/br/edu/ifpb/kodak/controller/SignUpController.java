b33f7f146c1cde8d7dfda2cf0bd145d44a56a5ec
buenorafa <rafaelbuenooo@outlook.com>
2025-01-18 17:29:28 -0300

    refactor: signup from photographercontroller -> signupcontroller

---
+++ src/main/java/br/edu/ifpb/kodak/controller/SignUpController.java
@@ -0,0 +1,40 @@
+package br.edu.ifpb.kodak.controller;
+
+import br.edu.ifpb.kodak.model.Photographer;
+import br.edu.ifpb.kodak.service.PhotographerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/sign-up")
+public class SignUpController {
+    @Autowired
+    private PhotographerService photographerService;
+
+    @GetMapping
+    public String getForm(Photographer photographer, Model model) {
+        model.addAttribute("photographer", photographer);
+        return "/sign-up/form";
+    }
+
+    @PostMapping
+    public ModelAndView savePhotographer(@Valid Photographer photographer, BindingResult result, ModelAndView model, RedirectAttributes attr){
+        if (result.hasErrors()) {
+            model.setViewName("/sign-up/form");
+            return model;
+
+        }
+        photographerService.savePhotographer(photographer);
+        attr.addFlashAttribute("mensagem", "Fotógrafo " + photographer.getName() + " " + "salvo" + "com sucesso!");
+        model.setViewName("redirect:/");
+        return model;
+    }
+}
