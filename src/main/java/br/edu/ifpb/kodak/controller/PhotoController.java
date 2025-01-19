362304318db4ec694d2cb497e234af408a9e5399
luna.sammuel <luna.sammuel@academico.ifpb.edu.br>
2025-01-19 23:24:11 +0000

    feat: created photo controller

--- src/main/java/br/edu/ifpb/kodak/controller/PhotoController.java
+++ src/main/java/br/edu/ifpb/kodak/controller/PhotoController.java
@@ -1,5 +1,42 @@
 package br.edu.ifpb.kodak.controller;
 
-public class PhotoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.ifpb.kodak.model.Photo;
+import br.edu.ifpb.kodak.model.Photographer;
+import br.edu.ifpb.kodak.service.PhotoService;
+import br.edu.ifpb.kodak.service.PhotographerService;
+
+@Controller
+@RequestMapping("/photo")
+public class PhotoController {
+    @Autowired
+    private PhotoService photoService;
+
+    @Autowired
+    private PhotographerService photographerService;
+
+    @GetMapping("/post")
+    public String postPage(@RequestParam("photoId") Integer photoId, Model model) {
+        Photo photo = photoService.getPhotoById(photoId)
+                .orElseThrow(() -> new RuntimeException("Foto não encontrada"));
+
+        model.addAttribute("photo", photo);
+
+        // Photographer photographer = photo.getPhotographer();
+
+        // model.addAttribute("photographer", photographer);
+
+        return "photo/post";
+    }
+    
 }
