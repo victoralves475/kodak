17d0b02da5db01640901158351f057bfae9826a3
islanpedro01 <islanpedro.12@hotmail.com>
2025-03-10 10:03:41 -0300

    hashtagController updated

--- src/main/java/br/edu/ifpb/kodak/controller/HashtagController.java
+++ src/main/java/br/edu/ifpb/kodak/controller/HashtagController.java
@@ -1,5 +1,15 @@
 package br.edu.ifpb.kodak.controller;
 
+import br.edu.ifpb.kodak.service.HashtagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tag")
 public class HashtagController {
+
+    @Autowired
+    private HashtagService hashtagService;
 
 }
