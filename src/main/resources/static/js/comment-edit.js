0ac7621c690c5b684407faaea8b660fb001b5ba0
buenorafa <rafaelbuenooo@outlook.com>
2025-03-11 00:46:12 -0300

    Adiciona verificações caso textarea esteja vazio e confirmações de exclusão do comentário

--- src/main/resources/static/js/comment-edit.js
+++ src/main/resources/static/js/comment-edit.js
@@ -6,3 +6,19 @@ function disableEdit(event, commentId){
     event.preventDefault();
     document.getElementById("edit-container-" + commentId).classList.add(["hidden"]);
 }
+
+function confirmEmptyComment(form) {
+    let textArea = form.querySelector("textarea[name='commentText']");
+    let commentText = textArea.value.trim(); // Remove espaços extras
+
+    if (commentText === "") {
+        let confirmDelete = confirm("O comentário está vazio. Deseja excluir?");
+        return confirmDelete; // Se o usuário confirmar, retorna true e o formulário será enviado
+    }
+
+    return true; // Continua normalmente se o comentário não estiver vazio
+}
+
+function confirmDelete(){
+ return confirm("Deseja realmente excluir este comentário?");
+}
