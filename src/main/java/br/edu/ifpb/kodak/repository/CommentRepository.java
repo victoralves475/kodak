6b93ce86967e926b3baa471ef049ba05b951c733
buenorafa <rafaelbuenooo@outlook.com>
2025-03-10 23:23:24 -0300

    Adiciona metodo deleteCommentById hard coded pq o spring não tava deletando

--- src/main/java/br/edu/ifpb/kodak/repository/CommentRepository.java
+++ src/main/java/br/edu/ifpb/kodak/repository/CommentRepository.java
@@ -5,9 +5,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifpb.kodak.model.Comment;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     List<Comment> findByPhotoId(int photoId);
+
+    @Modifying
+    @Query("DELETE FROM Comment c WHERE c.id = :id")
+    void deleteCommentById(@Param("id") Integer id);
 
 }
