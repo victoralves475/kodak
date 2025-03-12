a8a8d83a9400c16d2465bc8ea8fd4d209cb538b4
Victor <antoniovictoralves@gmail.com>
2025-03-12 13:55:03 -0300

    commit

--- src/main/java/br/edu/ifpb/kodak/service/HashtagService.java
+++ src/main/java/br/edu/ifpb/kodak/service/HashtagService.java
@@ -1,29 +1,41 @@
 package br.edu.ifpb.kodak.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.ifpb.kodak.model.Hashtag;
+import br.edu.ifpb.kodak.model.Photo;
 import br.edu.ifpb.kodak.repository.HashtagRepository;
 
 @Service
 public class HashtagService {
 
-    @Autowired
-    private HashtagRepository hashtagRepository;
+    private final HashtagRepository hashtagRepository;
+    private final PhotoService photoService;
 
-    public HashtagService(HashtagRepository hashtagRepository) {
+    @Autowired
+    public HashtagService(HashtagRepository hashtagRepository, PhotoService photoService) {
         this.hashtagRepository = hashtagRepository;
+        this.photoService = photoService;
     }
 
     // CRUD
 
     // Create
     public Hashtag create(Hashtag hashtag) {
+        String tagName = hashtag.getTagName().trim();
+        if (!tagName.startsWith("#")) {
+            tagName = "#" + tagName;
+        }
+        hashtag.setTagName(tagName);
         return hashtagRepository.save(hashtag);
     }
+
 
     // Read
     public Optional<Hashtag> getHashtagById(int id) {
@@ -36,7 +48,7 @@ public class HashtagService {
 
     // Find or Create
     public Hashtag findOrCreateByName(String tagName) {
-        return hashtagRepository.findByTagNameContainingIgnoreCase(tagName)
+        return hashtagRepository.findByTagNameIgnoreCase(tagName)
                 .orElseGet(() -> create(new Hashtag(tagName)));
     }
 
@@ -44,5 +56,30 @@ public class HashtagService {
     public void delete(Hashtag hashtag) {
         hashtagRepository.delete(hashtag);
     }
-	
+
+    // Remove hashtag from photo
+    @Transactional
+    public void removeHashtagFromPhoto(Integer photoId, Integer tagId) {
+        Photo photo = photoService.getPhotoById(photoId)
+                .orElseThrow(() -> new RuntimeException("Foto não encontrada"));
+
+        Hashtag hashtag = hashtagRepository.findById(tagId)
+                .orElseThrow(() -> new RuntimeException("Hashtag não encontrada"));
+
+        photo.getTags().remove(hashtag);
+
+        photoService.savePhoto(photo);
+    }
+
+    public List<String> searchTagsByPartialName(String partialName) {
+        return hashtagRepository.findAllByTagNameContainingIgnoreCase(partialName)
+                .stream()
+                .map(Hashtag::getTagName)
+                .collect(Collectors.toList());
+    }
+
+
+
+
+
 }
